40c64fdbc6753109e657613732637707d0cb64bc
Ja <ihunter@o2.pl>
2022-05-17 17:35:22 +0200

    splay implementation

--- Zad5/rotates.jl
+++ Zad5/rotates.jl
@@ -36,3 +36,30 @@ function right_rotate(T::SplayTree,z::SPT)
     y.right = z
     z.upper = y
 end
+
+function splay(tree::SplayTree, node::SPT)
+    while !isnothing(node.upper)
+        println("UMM?")
+        upper = node.upper
+        grand_upper = node.upper.upper
+        if isnothing(grand_upper)
+            if node == upper.left
+                right_rotate(tree, node.upper)
+            else
+                left_rotate(tree, node.upper)
+            end
+        elseif node == upper.left && upper == grand_upper.left
+            right_rotate(tree, grand_upper)
+            right_rotate(tree, upper)
+        elseif node == upper.right && upper == grand_upper.right
+            left_rotate(tree, grand_upper)
+            left_rotate(tree, upper)
+        elseif node == upper.right && upper == grand_upper.left
+            left_rotate(tree, node.upper)
+            right_rotate(tree, node.upper)
+        else
+            right_rotate(tree, node.upper)
+            left_rotate(tree, node.upper)
+        end
+    end
+end
